693c4ad1df46bd1240d7d9d76125996e15f3671e
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2022-12-30 21:01:17 +0100

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,43 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function () {});
+
+player.on(
+  'timeupdate',
+  throttle(function ({ seconds }) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
+
+// player.on(
+//   'timeupdate',
+//   throttle(function ({ seconds }) {
+//     localStorage.setItem('videoplayer-current-time', seconds);
+//   }, 1000)
+// );
+
+// const videoCurrentTime = () => {
+//   return localStorage.getItem('videoplayer-current-time') || 0;
+// };
+
+// player
+//   .setCurrentTime(videoCurrentTime())
+//   .then(function () {})
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         break;
+//       default:
+//         break;
+//     }
+//   });
+
+//   const videoCurrentTime = () => {
+//     return
+//   };
