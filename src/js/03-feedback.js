212b82176537c3af3bd83f162b03a590eecb3120
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2022-12-31 21:25:17 +0100

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
 import { throttle } from 'lodash';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const LOCAL_KEY = 'feedback-form-state';
+
+feedbackForm.addEventListener(
+  'input',
+  throttle(() => {
+    let storage = {
+      email: input.value,
+      message: textarea.value,
+    };
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(storage));
+  }, 500)
+);
+
+let parsed = JSON.parse(localStorage.getItem(LOCAL_KEY));
+
+if (localStorage.getItem(LOCAL_KEY) === null) {
+  input.value = '';
+  textarea.value = '';
+} else {
+  input.value = parsed.email;
+  textarea.value = parsed.message;
+}
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+  localStorage.removeItem(LOCAL_KEY);
+  input.value = '';
+  textarea.value = '';
+  console;
+});
